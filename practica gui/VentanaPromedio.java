16f43eb9d3e0038f2f68c47c1be2fd20776dc653
XxTheDemonxX <ericdaniel.barreto@gmail.com>
2026-03-03 22:46:44 -0500

    Add files via upload

---
+++ practica gui/VentanaPromedio.java
@@ -0,0 +1,18 @@
+package gui;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class VentanaPromedio extends JFrame{
+private JTextField txtMateria;
+private JTextField txtNombre;
+private JTextField txtNota1;
+private JTextField txtNota2;
+private JTextField txtNota3;
+private JButton btnCalcular;
+private JButton btnLimpiar;
+private JLabel lblResultado;
+}
